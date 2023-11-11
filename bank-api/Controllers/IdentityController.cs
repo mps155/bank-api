1d3cfe9a2e2ff27805315d6bf6b1edb011649c86
Michel Pereira dos Santos <michel.santos@e-construmarket.com.br>
2023-11-11 17:09:22 -0300

    improve

---
+++ bank-api/Controllers/IdentityController.cs
@@ -0,0 +1,25 @@
+﻿using bank_api.Models.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bank_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IdentityController : Controller
+    {
+        private readonly ILogger<IdentityController> _logger;
+
+        public IdentityController(ILogger<IdentityController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [ActionName("createUser")]
+        public dynamic createUser(newUserModel newUserModel)
+        {
+            
+            return Ok(newUserModel);
+        }
+    }
+}
---
+++ bank-api/Models/Identity/newUserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace bank_api.Models.Identity
+{
+    public class newUserModel
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string phone { get; set; }
+        public DateTime creationDate { get; set; }
+    }
+}
